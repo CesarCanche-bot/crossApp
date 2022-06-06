7c721d29eee97f5277153694cad0ed02b61b38b6
canche <canche@ensenada.gob.mx>
2022-06-06 12:57:10 -0700

    ADD new button and avoid endofline erros

---
+++ views/mainMenu/MainMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+const MainMenu = () => {
+  return (
+    <View>
+      <Button title="AMRAP" />
+    </View>
+  );
+};
+
+export default MainMenu;
