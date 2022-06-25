7d5f86d3bd9a72bcda4458c8b45f3b533595c507
canche <canche@ensenada.gob.mx>
2022-06-24 17:01:01 -0700

    ADD forTime component

---
+++ views/forTime/ForTime.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const TimersComponent = ({}) => {
+  return (
+    <View>
+      <Text>julio cesar</Text>
+    </View>
+  );
+};
+
+export default TimersComponent;
